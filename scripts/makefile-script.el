5b3a95a41da1aec0520915e444a852295dda9c5c
Yen-Chin Lee <coldnew.tw@gmail.com>
2017-07-11 13:17:29 +0800

    makefile-script.el: remove extra whitespace when gen html
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- scripts/makefile-script.el
+++ scripts/makefile-script.el
@@ -96,6 +96,20 @@
    (dot . t)
    (plantuml . t)))
 
+
+;; remove extra whitespace when generate html document
+(defadvice org-html-paragraph (before org-html-paragraph-advice
+				      (paragraph contents info) activate)
+  "Join consecutive Chinese lines into a single long line without
+unwanted space when exporting org-mode to html."
+  (let* ((origin-contents (ad-get-arg 1))
+	 (fix-regexp "[[:multibyte:]]")
+	 (fixed-contents
+	  (replace-regexp-in-string
+	   (concat
+	    "\\(" fix-regexp "\\) *\n *\\(" fix-regexp "\\)") "\\1\\2" origin-contents)))
+    (ad-set-arg 1 fixed-contents)))
+
 (defun generate-this-doc ()
   "Export the README.org to index.html."
   (find-file "README.org")
